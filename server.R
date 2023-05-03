da364e84bedc7cba2719344d2359e2ebf40a93ec
vizualizare <alexandru.dumitrescu@gmail.com>
2023-05-03 14:35:27 +0000

    update layout

--- server.R
+++ server.R
@@ -1,27 +1,31 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
 
-# Define server logic required to draw a histogram
-function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)', main = "")
-
-    })
-
+server <- function(input, output) {
+  
+  output$linePlots <- renderPlot({
+    obs_to_include <- as.integer(ChickWeight$Chick) <= input$numChicks
+    chicks <- ChickWeight[obs_to_include, ]
+    
+    ggplot(
+      chicks,
+      aes(
+        x = Time,
+        y = weight,
+        group = Chick
+      )
+    ) +
+      geom_line(alpha = 0.5) +
+      ggtitle("Chick weights over time")
+  })
+  
+  output$dists <- renderPlot({
+    ggplot(
+      ChickWeight,
+      aes(x = weight)
+    ) +
+      facet_wrap(input$distFacet) +
+      geom_density(fill = "#fa551b", color = "#ee6331") +
+      ggtitle("Distribution of weights by diet")
+  })
+  
 }
--- ui.R
+++ ui.R
@@ -1,41 +1,70 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
 
-# Define UI for application that draws a histogram
-fluidPage(
+ui <- navbarPage(
+  title = "LTSER Explorer",
+  selected = "Line Plots",
+  collapsible = TRUE,
   theme = my_theme,
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-    
-    # https://rstudio.github.io/bslib/articles/cards.html
-    mainPanel(
-      card(
-        height = 450, 
-        full_screen = TRUE,
-        card_header("Histogram of waiting times"),
-        card_body(
-          plotOutput("distPlot")
+  tabPanel(
+    title = "Line Plots",
+    grid_container(
+      row_sizes = c(
+        "1fr"
+      ),
+      col_sizes = c(
+        "250px",
+        "1fr"
+      ),
+      gap_size = "10px",
+      layout = c(
+        "num_chicks linePlots"
+      ),
+      grid_card(
+        area = "num_chicks",
+        card_header("Settings"),
+        card_body_fill(
+          sliderInput(
+            inputId = "numChicks",
+            label = "Number of chicks",
+            min = 1,
+            max = 15,
+            value = 5,
+            step = 1,
+            width = "100%"
+          )
+        )
+      ),
+      grid_card_plot(area = "linePlots")
+    )
+  ),
+  tabPanel(
+    title = "Distributions",
+    grid_container(
+      row_sizes = c(
+        "165px",
+        "1fr"
+      ),
+      col_sizes = c(
+        "1fr"
+      ),
+      gap_size = "10px",
+      layout = c(
+        "facetOption",
+        "dists"
+      ),
+      grid_card_plot(area = "dists"),
+      grid_card(
+        area = "facetOption",
+        card_header("Distribution Plot Options"),
+        card_body_fill(
+          radioButtons(
+            inputId = "distFacet",
+            label = "Facet distribution by",
+            choices = list("Diet Option" = "Diet", "Measure Time" = "Time")
+          )
         )
       )
     )
-  )
+  ),
+  tabPanel(title = "Empty Tab")
 )
