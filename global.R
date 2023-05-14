14aa8d40a0255620cd26339f77aab22ce5cd6191
vizualizare <alexandru.dumitrescu@gmail.com>
2023-05-14 16:21:35 +0000

    add ads function and libraries

--- global.R
+++ global.R
@@ -5,6 +5,12 @@ library(bslib)
 library(sf)
 library(leaflet)
 library(htmltools)
+library(dplyr)
+library(RColorBrewer)
+library(terra)
+
+source("utils/leaflet_fun.R")
+source("utils/map_fun_cols.R")
 
 library(showtext) # Needed for custom font support
 # Setup the bslib theme object
@@ -16,4 +22,15 @@ my_theme <-
 
 ltser <- st_read("www/data/shp/ltser.topojson", quiet = T)
 
+
+# selectare perioada de interes
+select_period <- read.csv("www/data/tabs/select_period.csv") 
+select_period <- setNames(select_period$choice, select_period$parameter)
+
+choices_map_monthly <- read.csv("www/data/tabs/slelect_input_parameters_monthly.csv") 
+choices_map_monthly <- setNames(choices_map_monthly$choice, choices_map_monthly$parameter)
+
+# citeste produse
+ssm <- terra::rast("www/data/ncs/ssm_ltser_mon.nc")
+dats.ssm <- as.Date(names(ssm) %>% gsub("ssm_days=", "",.) %>% as.integer(), origin = "1970-1-1 00:00:00") |> rev()
 
