41ddfff7c8ecd910e2cb2dfd291023a7bf2e4a59
vizualizare <alexandru.dumitrescu@gmail.com>
2023-05-16 20:05:58 +0000

    update template

--- ui.R
+++ ui.R
@@ -1,86 +1,76 @@
-
-ui <- navbarPage(
+# reconfigurare menu https://rstudio.github.io/bslib/reference/navs.html#details
+ui <- page_navbar(
   title = "LTSER Explorer",
   selected = "Maps",
   collapsible = TRUE,
+  fluid = T,
+  inverse = F,
   theme = my_theme,
-  tabPanel(
+  fill_mobile = T,
+  nav_panel(
     title = "Maps",
-    grid_container(
-      row_sizes = c(
-        "1fr"
-      ),
-      col_sizes = c(
-        "250px",
-        "1fr"
-      ),
-      gap_size = "10px",
-      layout = c(
-        "transp_ind map_ltser"
-      ),
-      grid_card(
-        area = "transp_ind",
-        card_header("Settings"),
-        card_body_fill(
-          sliderInput(
-            inputId = "transp_ind",
-            label = "Transparency LTSER",
-            min = 0,
-            max = 1,
-            value = 0.5,
-            step = 0.1,
-            width = "100%"
-          ),
-          selectInput(
-            "parameter_monthly", "Parameter:", 
-            choices_map_monthly, 
-            selected = choices_map_monthly[2]
-          ),
-          selectInput(
-            inputId = 'month_indicator',
-            label = 'Month:',
-            rev(dats.ssm) |> format("%Y %b"),
-            selected = max(dats.ssm) |> format("%Y %b")
-          ),
-          radioButtons( # radio button show values
-            "radio_mon", label = "Click on map behavior",
-            choices = 
-              list(
-                "Display current values on popup" = 1, 
-                "Plot timeseries (below map)" = 2
-              ), 
-            selected = 1
+    fluidRow(
+      column(
+        width = 2,
+        card(
+          card_body(
+            
+            sliderInput(
+              inputId = "transp_ind",
+              label = "Transparency LTSER",
+              min = 0,
+              max = 1,
+              value = 0.5,
+              step = 0.1,
+              width = "100%"
+            ),
+            selectInput(
+              "parameter_monthly", "Parameter:", 
+              choices_map_monthly, 
+              selected = choices_map_monthly[2]
+            ),
+            selectInput(
+              inputId = 'month_indicator',
+              label = 'Month:',
+              rev(dats.ssm) |> format("%Y %b"),
+              selected = max(dats.ssm) |> format("%Y %b")
+            ),
+            radioButtons( # radio button show values
+              "radio_mon", label = "Click on map behavior",
+              choices = 
+                list(
+                  "Display current values on popup" = 1, 
+                  "Plot timeseries (below map)" = 2
+                ), 
+              selected = 1
+            )
           )
         )
       ),
-      grid_card(
-        area = "map_ltser",
-        card_header("LTSER location"),
-        card_body(
-          # fill = T,
-          # full_screen = T,
-          leafletOutput("map_ltser", height = 450)
-        ),
-        
-        #fill = T,
-        #full_screen = T,
-        conditionalPanel( # show graphs only when data available
-          condition = "input.radio_mon == 2 && output.condpan_monthly != 'nas'",
+      column(
+        width = 7,
+        card(
           card_body(
-            highchartOutput("rs_mon", height = "350px")# %>% withSpinner(size = 0.5)
-          )
-        ),
-        conditionalPanel(
-          condition = "input.radio_mon == 2 && output.condpan_monthly == 'nas'",
+            leafletOutput("map_ltser")
+            ),
           card_body(
-            p("You must click on an area with indicator values available")
+            conditionalPanel( # show graphs only when data available
+              condition = "input.radio_mon == 2 && output.condpan_monthly != 'nas'",
+              highchartOutput("rs_mon", height = "350px")# %>% withSpinner(size = 0.5)
+            ),
+            conditionalPanel(
+              condition = "input.radio_mon == 2 && output.condpan_monthly == 'nas'",
+              p("You must click on an area with indicator values available")
+            )
           )
         )
       )
     )
   ),
-  tabPanel(
+  nav_panel(
     title = "Other section"
   ),
-  tabPanel(title = "About")
+  nav_panel(
+    title = "About"
+  )
 )
