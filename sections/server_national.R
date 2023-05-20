b332af8b9bac7de440c0a46cdfaba3c6194c253b
vizualizare <alexandru.dumitrescu@gmail.com>
2023-05-20 10:25:00 +0000

    add starter graph

--- sections/server_national.R
+++ sections/server_national.R
@@ -1,6 +1,7 @@
 
 reac_rs_mon <- reactive ({
   
+ 
   index <- which(format(dats.ssm, "%Y %b") %in% input$month_indicator)
   indicator <- input$parameter_monthly
   # indicator <- "mm" 
@@ -81,6 +82,19 @@ observe({
 
 # reactive values pentru plot lst time series din raster
 values_plot_lst_mon <- reactiveValues(input = NULL, title = NULL, cors = NULL)
+
+observe({
+  isolate(print(input$tab_maps))
+  var <- reac_rs_mon()$indicator
+  lon = 25
+  lat = 46
+  dd <- extract_point(fname =  paste0("www/data/ncs/",  var, "_ltser_mon.nc"), lon  = lon, lat = lat, variable = var) 
+  print(head(dd))
+  ddf <- data.frame(date = as.Date(names(dd)), value = round(dd, 1)) %>% slice(1:reac_rs_mon()$index)
+  values_plot_lst_mon$input <- ddf
+  values_plot_lst_mon$title <- paste0("Extracted value ",toupper(var)," values for point lon = ",round(lon, 5)," lat = "  , round(lat, 5))
+})
+
 # interactivitate raster
 observe({
   proxy <- leafletProxy("map_ltser")
@@ -92,16 +106,16 @@ observe({
   if (input$radio_mon == 1 & !is.null(click)) {
     show_pop(var = var,x = click$lng, y = click$lat, rdat = rs, proxy = proxy)
   } else {
+   
     proxy %>% clearPopups()
     if (!is.null(click)) {
-      
       cell <- terra::cellFromXY(rs, cbind(click$lng, click$lat))
       xy <- terra::xyFromCell(rs, cell)
       dd <- extract_point(fname = fil.nc , lon = xy[1], lat = xy[2], variable = var) 
       # pentru afisare conditional panel si titlu grafic coordonates
       condpan_monthly.txt <- ifelse(
         is.na(mean(dd, na.rm = T)) | is.na(cell), 
-        "nas", 
+        "You must click on an area with indicator values available", 
         paste0("Extracted value ",toupper(var)," values for point lon = ",round(click$lng, 5)," lat = "  , round(click$lat, 5))
       )
       output$condpan_monthly <- renderText({
@@ -121,8 +135,8 @@ observe({
 
 # plot actualizat daca schimb si coordonatee
 output$rs_mon <- renderHighchart({
-  req(!is.na(values_plot_lst_mon$input))
-  indicator <-reac_rs_mon()$indicator
+  req(values_plot_lst_mon$input)
+  indicator <- reac_rs_mon()$indicator
   
   ytitle <- ifelse(indicator %in% c("ssm"),"%")
 
