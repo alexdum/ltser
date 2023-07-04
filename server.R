a04b63ad1612785b3bcb02ce7a8fe37f135ae87e
vizualizare <alexandru.dumitrescu@gmail.com>
2023-07-04 11:39:29 +0000

    add socio

--- server.R
+++ server.R
@@ -5,5 +5,6 @@ server <- function(input, output,  session) {
   source(file = "sections/server_national.R", local = T)
   source(file = "sections/server_admin.R", local = T)
   source(file = "sections/server_insitu.R", local = T)
+  source(file = "sections/server_social.R", local = T)
   
 }
