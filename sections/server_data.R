acc7f7615adff70e8d23cdfd73d9595f39b32e22
procesare <alexandru.dumitrescu@gmail.com>
2023-09-23 11:05:50 +0000

    siplay hourly

--- sections/server_data.R
+++ sections/server_data.R
@@ -1,28 +1,9 @@
-
-# pentru selectare perioada timp sepcifica fiecarui parametru
-observeEvent(input$temporal_resolution, {
-  choice_filt <- 
-    switch(
-      input$temporal_resolution,
-      hourly = select_meteo_hourly,
-      daily = select_meteo_daily
-    )
-  updateSelectInput(
-    session, "parameter_meteo",
-    choices = choice_filt,
-    selected = choice_filt[2]
-  )
-  
-})
-
-
-
 data_sel <- reactive({
   net <- input$network_data
   # selectie unitate
   switch( # alege nume indicator care să fie afișat
     which(c("ws", "ec", "bu") %in%  net),
-    admin_spat <- ws,
+    admin_spat <- ws
   )
   data_sub <- # select dataset
     switch(
@@ -30,25 +11,31 @@ data_sel <- reactive({
       hourly = hourly,
       daily = daily
     )
-  timesel_sub <-  # select time slice
+  param_sub <-  # select time slice
+    switch(
+      input$temporal_resolution,
+      hourly = input$parameter_meteo,
+      daily =  gsub("01h", "24h", input$parameter_meteo)
+    )
+  
+  timesel_sub <-  # select aparm of interest
     switch(
       input$temporal_resolution,
       hourly = input$datetime_meteo,
       daily = input$date_meteo
     )
-  print(input$parameter_meteo)
-  print(timesel_sub)
-  
-  data_sub <- data_sub |> 
-    filter(
-      time == timesel_sub,
-      variable == input$parameter_meteo
-    )  |> collect()
-  
+ 
   print(data_sub)
+  data_sel <- data_sub |> 
+    mutate(time = time) |>
+    filter(
+      variable == param_sub,
+      time %in% timesel_sub
+    ) |> collect()
   
-  admin_spat <- admin_spat |> inner_join(data_sub, by = c("Name" = "id"))
- # print(admin_spat )
+ 
+  admin_spat <- admin_spat |> inner_join(data_sel, by = c("Name" = "id"))
+  # print(admin_spat )
   
   
   list(
@@ -60,9 +47,9 @@ data_sel <- reactive({
 
 # harta leaflet -----------------------------------------------------------
 output$map_data <- renderLeaflet({
-  qpal <- colorQuantile("YlOrRd", data_sel()$admin_spat$values, n = 3)
+  qpal <- colorQuantile("YlOrRd", isolate(data_sel()$admin_spat$values), n = 3)
   leaflet_fun_data(
-    data = isolate(data_sel()$admin_spat), qpal = qpal
+    data = isolate(data_sel()$admin_spat), qpal = isolate(qpal)
   )
 })
 
@@ -84,14 +71,15 @@ observe({
       layerId = ~Name
       #clusterOptions = markerClusterOptions(freezeAtZoom = T) 
     )  |>
-    # addLabelOnlyMarkers(
-    #   label = ~values,
-    #   labelOptions = labelOptions(
-    #     noHide = TRUE, textOnly = TRUE,
-    #     direction = "center", offset = c(0,0), sticky = T,
-    #     fontsize = 14
-    #   )
-    #) |> # pentru zoom limite retea incarcata
+    clearMarkers() |>
+    addLabelOnlyMarkers(
+      label = ~values,
+      labelOptions = labelOptions(
+        noHide = TRUE, textOnly = TRUE,
+        direction = "center", offset = c(0,0), sticky = T,
+        fontsize = 14
+      )
+    ) |> # pentru zoom limite retea incarcata
     fitBounds(bbox[1], bbox[2], bbox[3], bbox[4]) 
 })
 
