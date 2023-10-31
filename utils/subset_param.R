119387a5fb5312ea1d2020f98338e34f8925663d
procesare <alexandru.dumitrescu@gmail.com>
2023-10-31 20:27:07 +0000

    add soil parameters

--- utils/subset_param.R
+++ utils/subset_param.R
@@ -1,10 +1,12 @@
 subset_param  <- function(input) {
   subset_param <- switch(
-    which(c("tm", "ws", "ps", "hu") %in% substr(input, 1, 2)),
+    which(c("tm", "ws", "ps", "hu", "ts", "sh") %in% substr(input, 1, 2)),
     c("tm", "ws"),
     c("tm", "ws"),
     c("ps", "hu"),
-    c("ps", "hu")
+    c("ps", "hu"),
+    c("ts", "sh"),
+    c("ts", "sh")
   )
   return(subset_param)
 }
