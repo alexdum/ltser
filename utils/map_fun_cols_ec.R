8d4cf7e9fce3d8e04fe8176c708207bded9a3c0b
procesare <alexandru.dumitrescu@gmail.com>
2023-11-26 15:42:12 +0000

    different for  net radiation

--- utils/map_fun_cols_ec.R
+++ utils/map_fun_cols_ec.R
@@ -2,13 +2,20 @@ mapa_fun_cols_ec <- function(indic = NA,  domain = NA, title = NA, nbins) {
 
   
   
-  if (indic  %in% c("co2_flux", "ch4_flux", "LE","H", "RN_1_1_1", "RH_1_1_1","ET", "PPFD_1_1_1")) { # pentru toate temperaturile
-   
+  if (indic  %in% c("co2_flux", "ch4_flux", "LE","H", "RH_1_1_1","ET", "PPFD_1_1_1")) { # pentru toate temperaturile
     pal <- colorBin(palette = "BuPu",domain = domain, bins = nbins)
     pal2 <- colorBin(palette = "BuPu",domain = domain, reverse = T, bins = nbins)
     leaflet_titleg <- paste0("<html>", gsub(",","",toString(rep("&nbsp;", 2))), title,"</html>")
+  }
+  
+  if (indic  %in% c("RN_1_1_1")) { # pentru toate temperaturile
+    
+    pal <- colorBin(palette = "OrRd",domain = domain, bins = nbins)
+    pal2 <- colorBin(palette = "OrRd",domain = domain, reverse = T, bins = nbins)
+    leaflet_titleg <- paste0("<html>", gsub(",","",toString(rep("&nbsp;", 2))), title,"</html>")
     
   }
+  
   
   
   return(list(pal = pal, pal_rev = pal2, tit_leg = leaflet_titleg))
