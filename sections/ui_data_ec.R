54c32d1acaad92d259d5e779ad8ab8d850351b56
procesare <alexandru.dumitrescu@gmail.com>
2023-11-26 12:39:19 +0000

    add time controler

--- sections/ui_data_ec.R
+++ sections/ui_data_ec.R
@@ -15,6 +15,23 @@ filters_data_ec <- list(
     "parameter_ec", "Parameter:", 
     select_ec_halfhourly, 
     selected = select_ec_halfhourly[1]
+  ),
+  
+  shinyWidgets::airDatepickerInput(
+    inputId = "datetime_ec",
+    label = "Date/Time:",
+    multiple = F,
+    value = as.character(max(hhourly_dats$time_eet)), # transforma in caracater sa nu le decaleze
+    minDate = as.character(min(hhourly_dats$time_eet)),
+    maxDate = as.character(max(hhourly_dats$time_eet)),
+    timepicker = TRUE,
+    autoClose = T,
+    timepickerOpts =  shinyWidgets::timepickerOptions(
+      hoursStep = 1,
+      timeFormat = "HH:mm",
+      minutesStep = 30
+    )
+    
   )
 )
 
@@ -28,17 +45,17 @@ data_level_ec <- card(
     layout_columns(
       card(
         full_screen = T,
-       "tba"
+        "tba"
       ),
       card(
         full_screen = T,
-       "tba"
+        "tba"
       )
-      ),
+    ),
     accordion(
       open = F,
       accordion_panel(
-       "tba"
+        "tba"
         
       )
     )
